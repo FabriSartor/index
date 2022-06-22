ec224f8e9a7d1aa62d4242f66317f6f469f7898f
fabrisartor <fabriziosartorsaita@gmail.com>
2022-06-22 18:45:12 -0300

    fabri

---
+++ js/formulario.js
@@ -0,0 +1,21 @@
+// alert("no presione enter");
+// function alert(){
+//     var ayuda=document.getElementById("claro")
+//     ayuda.innerHTML="ingrese su nombre correctamente"
+// }
+// function alert1(){
+//     var ayuda=document.getElementById("claro")
+//     ayuda.innerHTML="ingrese su apellido correctamente"
+// }
+// function alert2(){
+//     var ayuda=document.getElementById("claro")
+//     ayuda.innerHTML="ingrese su email correctamente"
+// }
+// function alert3(){
+//     var ayuda=document.getElementById("claro")
+//     ayuda.innerHTML="ingrese su contraseña correctamente"
+// }
+// function alert4(){
+//     var ayuda=document.getElementById("claro")
+//     ayuda.innerHTML="seleccione el archivo correctamente"
+// }
