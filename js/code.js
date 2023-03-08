b5ade45bab2439245e416792b6bdb082aa9bb642
fabrisartor <fabriziosartorsaita@gmail.com>
2023-03-08 16:12:04 -0300

    cambios nuevos

--- js/code.js
+++ js/code.js
@@ -1,38 +1,52 @@
-function preguntarNombreYApellido() {
-    var nombre=prompt("ingrese su nombre");
-    var apellido=prompt("ingrese su apellido");
-    alert (`hola ${nombre} ${apellido}`); 
-}
-function mayus(){
-    var datospersonales=document.getElementById("Datos");
-    datospersonales.innerHTML="DATOS PERSONALES";
-    datospersonales.style.backgroundColor="lightblue";
-}
-function minus(){
-    var datospersonales=document.getElementById("gatos");
-    datospersonales.innerHTML="EDUCACION";
-}
-function colour1(){
-    var oscuro=document.getElementById("oscuro");
-    oscuro.style.backgroundColor="black";
-}
-function colour2(){
-    var oscuro=document.getElementById("oscuro");
-    oscuro.style.backgroundColor="white";
-}
-function buttonblack(){
-    var button=document.getElementById("merca")
-    button.style.backgroundColor="black"
-}
-function buttonblack1(){
-    var boton=document.getElementById("merca1")
-    boton.style.backgroundColor="black"
-}
-function buttonblack2(){
-    var negro=document.getElementById("merca2")
-    negro.style.backgroundColor="black"
-}
-function buttonblack3(){
-    var negro=document.getElementById("merca3")
-    negro.style.backgroundColor="black"
-}
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+});
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
+
+
+
+// function preguntarNombreYApellido() {
+//     var nombre=prompt("ingrese su nombre");
+//     var apellido=prompt("ingrese su apellido");
+//     alert (`hola ${nombre} ${apellido}`); 
+// }
+// function mayus(){
+//     var datospersonales=document.getElementById("Datos");
+//     datospersonales.innerHTML="DATOS PERSONALES";
+//     datospersonales.style.backgroundColor="lightblue";
+// }
+// function minus(){
+//     var datospersonales=document.getElementById("gatos");
+//     datospersonales.innerHTML="EDUCACION";
+// }
+// function colour1(){
+//     var oscuro=document.getElementById("oscuro");
+//     oscuro.style.backgroundColor="black";
+// }
+// function colour2(){
+//     var oscuro=document.getElementById("oscuro");
+//     oscuro.style.backgroundColor="white";
+// }
+// function buttonblack(){
+//     var button=document.getElementById("merca")
+//     button.style.backgroundColor="black"
+// }
+// function buttonblack1(){
+//     var boton=document.getElementById("merca1")
+//     boton.style.backgroundColor="black"
+// }
+// function buttonblack2(){
+//     var negro=document.getElementById("merca2")
+//     negro.style.backgroundColor="black"
+// }
+// function buttonblack3(){
+//     var negro=document.getElementById("merca3")
+//     negro.style.backgroundColor="black"
+// }
