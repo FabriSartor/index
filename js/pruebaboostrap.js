568d280f231f1468032386d10e80d1f0ec3fc86e
fabrisartor <fabriziosartorsaita@gmail.com>
2023-03-08 10:27:27 -0300

    mensaje

---
+++ js/pruebaboostrap.js
@@ -0,0 +1,11 @@
+var fila1=document.getElementById("fila1");
+fila1.style.display = "none";
+var fila1=document.getElementById("fila2");
+fila1.style.display = "none";
+var fila1=document.getElementById("fila3");
+fila1.style.display = "none";
+var fila1=document.getElementById("fila4");
+fila1.style.display = "none";
+
+var alumno=document.getElementById("alumno");
+alumno.onclick=mostrarfilauno;
